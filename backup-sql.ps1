44008474c3540d5e167cbcbf5c76abf2c8078f7b
floalt <53464175+floalt@users.noreply.github.com>
2019-10-11 22:30:54 +0200

    v8.2
    Adding Comments
    Correct 24h time format in line 27 and 30

--- backup-sql.ps1
+++ backup-sql.ps1
@@ -2,22 +2,30 @@
 # author flo.alt@fa-netz.de
 # ver 0.81
 
+### setup your parameters here:
+
 param(
 $sqlinstance = "<servername\sql-instance>",
 $dbname = "<name of the database>"
 )
 
+### script start here
+
+# get the backup destination path
 $dbpath = (Get-SqlDatabase -ServerInstance $sqlinstance -Name $dbname).PrimaryFilePath
 $bakpath = $dbpath.replace("DATA","Backup")
 
+# delete old backup
 del $bakpath\$dbname-01.bak
 ren $bakpath\$dbname.bak $bakpath\$dbname-01.bak
 
+# perform the database backup
 Backup-SqlDatabase -ServerInstance $sqlinstance -Database $dbname -BackupAction Database
 
+# check if error
 if ($? -eq "True") {
-    echo "Last sucessful backup:" (Get-Date -Format dd.MM.yyy-hh:mm:ss) > "$bakpath\succesful.txt"
+    echo "Last backup succesful:" (Get-Date -Format dd.MM.yyy-HH:mm:ss) > "$bakpath\succesful.txt"
     }
 else {
-    echo "Last failure backup:" (Get-Date -Format dd.MM.yyy-hh:mm:ss) > "$bakpath\error.txt"
+    echo "Last backup fails:" (Get-Date -Format dd.MM.yyy-HH:mm:ss) > "$bakpath\error.txt"
     }
